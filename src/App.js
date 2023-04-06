81daf513b08169b51a1dcd89d809c571db081fdb
hashinisraq <hashinisraq.hi@gmail.com>
2023-04-07 00:19:35 +0600

    working folders

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
+import Header from "./Pages/Shared/Header";
+import Footer from "./Pages/Shared/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />}>
           </Route>
-          <Route path="home" element={<Home />}>
+          <Route path="/home" element={<Home />}>
           </Route>
           <Route path="*" element={<NotFound />}>
           </Route>
         </Routes>
       </Router>
+      <Footer />
     </>
   );
 }
--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Error...</h2>
+        <div className="text-center p-5 m-5">
+            <iframe title="wrong" src="https://giphy.com/embed/Wq9RLX06zRg4UM42Qf" ></iframe>
+            <h2>Page Not Found</h2>
+            <Link to="/home"><Button variant="secondary">Home</Button></Link>
         </div>
     );
 };
