bcbdf75b14e339860293b5da4dad1b1593b50fc2
hashinisraq <hashinisraq.hi@gmail.com>
2023-04-19 17:59:56 +0600

    modified

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Container, Form, InputGroup, Modal, Nav, Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import logo from "./images/logo.png";
@@ -8,26 +8,34 @@ import "./Home.css";
 
 
 const Header = () => {
+    // Search (Modal)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" variant="light">
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Container className="header-container">
-                    <Nav className="social-container">
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav.Link href="https://facebook.com">
-                                <FontAwesomeIcon icon={faFacebookF} className="icons" style={{ "color": "#175BEB" }} />
-                            </Nav.Link>
-                            <Nav.Link href="https://twitter.com">
-                                <FontAwesomeIcon icon={faTwitter} className="icons" style={{ "color": "#1DA1F2" }} />
-                            </Nav.Link>
-                            <Nav.Link href="https://instagram.com">
-                                <FontAwesomeIcon icon={faInstagram} className="icons" style={{ "color": "#F248BD" }} />
-                            </Nav.Link>
-                            <Nav.Link href="https://linkedin.com">
-                                <FontAwesomeIcon icon={faLinkedin} className="icons" style={{ "color": "#1DA1F2" }} />
-                            </Nav.Link>
-                        </Navbar.Collapse>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Nav>
+                        <Nav className="social-container">
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav.Link href="https://facebook.com">
+                                    <FontAwesomeIcon icon={faFacebookF} className="icons" style={{ "color": "#175BEB" }} />
+                                </Nav.Link>
+                                <Nav.Link href="https://twitter.com">
+                                    <FontAwesomeIcon icon={faTwitter} className="icons" style={{ "color": "#1DA1F2" }} />
+                                </Nav.Link>
+                                <Nav.Link href="https://instagram.com">
+                                    <FontAwesomeIcon icon={faInstagram} className="icons" style={{ "color": "#F248BD" }} />
+                                </Nav.Link>
+                                <Nav.Link href="https://linkedin.com">
+                                    <FontAwesomeIcon icon={faLinkedin} className="icons" style={{ "color": "#1DA1F2" }} />
+                                </Nav.Link>
+                            </Navbar.Collapse>
+                        </Nav>
                     </Nav>
 
                     <Navbar.Brand href="/home">
@@ -35,11 +43,44 @@ const Header = () => {
                     </Navbar.Brand>
 
 
-                    <Button className="search-btn">
+                    <Button className="search-btn" onClick={handleShow}>
                         <img src={search} alt="search icon" srcset="" width="20rem" />
                     </Button>
                 </Container>
             </Navbar>
+
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+            >
+                <Container style={{ "backgroundColor": "white", "color": "black", "borderRadius": "10px" }}>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">Enter your search</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                placeholder="Type to start your search"
+                                aria-label="search"
+                                aria-describedby="basic-addon2"
+                                style={{ "border": "1px solid #FF4C60" }}
+                            />
+                            <Button variant="outline-danger" id="button-addon2">
+                                Search
+                            </Button>
+                        </InputGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="dark" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Container>
+            </Modal>
         </div>
     );
 };
