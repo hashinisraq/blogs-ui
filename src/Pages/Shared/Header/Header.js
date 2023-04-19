c33889c854cea841f0ad2f21a9e268c2fdcd847a
hashinisraq <hashinisraq.hi@gmail.com>
2023-04-19 15:10:20 +0600

    header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import logo from "./images/logo.png";
 import search from "./images/search.png";
 import "./Home.css";
+
 
 const Header = () => {
     return (
         <div>
             <div className="container header-container">
                 <div className="social-container">
-                    <li>
-                        Facebook
-                    </li>
-                    <li>
-                        Twitter
-                    </li>
-                    <li>
-                        Instagram
-                    </li>
-                    <li>
-                        Linkedin
-                    </li>
+                    <FontAwesomeIcon icon={faFacebookF} className="icons" style={{ "color": "#175BEB" }} />
+
+                    <FontAwesomeIcon icon={faTwitter} className="icons" style={{ "color": "#1DA1F2" }} />
+
+                    <FontAwesomeIcon icon={faInstagram} className="icons" style={{ "color": "#F248BD" }} />
+
+                    <FontAwesomeIcon icon={faLinkedin} className="icons" style={{ "color": "#1DA1F2" }} />
+
                 </div>
                 <div>
                     <a href="/home">
